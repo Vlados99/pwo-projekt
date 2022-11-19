0561d6a7a801363b347c85247f7c70ba33be0c99
666666bg <65243723+666666bg@users.noreply.github.com>
2022-11-19 22:17:13 +0100

    fix test

--- pwo-project/src/test/java/project/RectangleTest.java
+++ pwo-project/src/test/java/project/RectangleTest.java
@@ -1,3 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ *
+ * @author vladi
+ */
 
 class RectangleTest {
 
