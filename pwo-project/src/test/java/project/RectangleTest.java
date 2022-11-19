0cb4315acaad8b6b1017e01fc3a97d782254a077
666666bg <65243723+666666bg@users.noreply.github.com>
2022-11-19 22:09:50 +0100

    add test

---
+++ pwo-project/src/test/java/project/RectangleTest.java
@@ -0,0 +1,28 @@
+
+class RectangleTest {
+
+    @Test
+    void shouldReturnTheBase() {
+        Rectangle rectangle = new Rectangle(2, 4);
+        assertEquals(rectangle.getBase(), 2);
+    }
+
+    @Test
+    void theHeightShouldBeEqualToTheSide() {
+        Rectangle rectangle = new Rectangle(2, 4);
+        assertEquals(rectangle.getHeight(), 4);
+    }
+
+    @Test
+    void shouldReturnThePerimeter() {
+        Rectangle rectangle = new Rectangle(2, 4);
+        assertEquals(rectangle.perimeter(), 12);
+    }
+
+    @Test
+    void shouldReturnTheArea() {
+        Rectangle rectangle = new Rectangle(2, 4);
+        assertEquals(rectangle.area(), 8);
+    }
+
+}
